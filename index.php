63cd8f33ada460e07ea51b596d167952ca484c60
zamaylaxelle29 <alexellezamayla05@gmail.com>
2024-11-07 00:57:50 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Ubuntu Server-PHP deployment</title>
+</head>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<body>
+
+
+    <p>Ubuntu Server-PHP deployment- Alexelle Zamayla</p>
+</body>
+
+<div class="container">
+	<label>Firstname:</label>
+	<input type="text" name="firstname" class="form-control">
+</div>
+<div class="container">
+	<label>Middlename:</label>
+	<input type="text" name="firstname" class="form-control">
+</div>
+<div class="container">
+	<label>Lastname:</label>
+	<input type="text" name="firstname" class="form-control">
+</div>
+</html>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
